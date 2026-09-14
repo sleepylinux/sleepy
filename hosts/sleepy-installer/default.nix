64c058b870c1e70ac8cf3e2162374418cfc56d2e
23iq <steambuyer69@gmail.com>
2026-09-14 22:17:54 +0200

    style: group installer hardware and boot settings

--- hosts/sleepy-installer/default.nix
+++ hosts/sleepy-installer/default.nix
@@ -17,15 +17,16 @@
   services.openssh.enable = lib.mkForce false;
   # The installer only needs a firmware framebuffer and network access. Full
   # desktop/GPU firmware is still selected by the installed hardware profile.
-  hardware.enableRedistributableFirmware = lib.mkForce false;
-  hardware.wirelessRegulatoryDatabase = true;
-  hardware.firmware = [
-    (import ../../packages/vendor/installer-network-firmware {inherit pkgs;})
-    pkgs.ipw2200-firmware
-    pkgs.rtl8192su-firmware
-    pkgs.zd1211fw
-  ];
-  boot.blacklistedKernelModules = ["amdgpu" "radeon" "nouveau" "nvidia" "i915" "xe"];
+  hardware = {
+    enableRedistributableFirmware = lib.mkForce false;
+    wirelessRegulatoryDatabase = true;
+    firmware = [
+      (import ../../packages/vendor/installer-network-firmware {inherit pkgs;})
+      pkgs.ipw2200-firmware
+      pkgs.rtl8192su-firmware
+      pkgs.zd1211fw
+    ];
+  };
   services.getty.helpLine = lib.mkForce "Sleepy installation and recovery · run sleepy-install";
   programs.bash.interactiveShellInit = ''
     if [ "$(tty)" = /dev/tty1 ] && [ -z "''${SLEEPY_INSTALLER_STARTED:-}" ]; then
@@ -39,8 +40,11 @@
     squashfsCompression = "zstd -Xcompression-level 15";
   };
   image.baseName = lib.mkForce "sleepy-0.1.0-alpha-x86_64-linux";
-  boot.supportedFilesystems = lib.mkForce ["btrfs" "vfat" "ext4"];
-  boot.kernelParams = ["console=tty0" "console=ttyS0,115200n8"];
+  boot = {
+    blacklistedKernelModules = ["amdgpu" "radeon" "nouveau" "nvidia" "i915" "xe"];
+    supportedFilesystems = lib.mkForce ["btrfs" "vfat" "ext4"];
+    kernelParams = ["console=tty0" "console=ttyS0,115200n8"];
+  };
   documentation = {
     enable = lib.mkForce false;
     nixos.enable = lib.mkForce false;
