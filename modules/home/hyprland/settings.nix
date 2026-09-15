bfa319a6fba977ebd61e172aabd7163c10faa9d5
23iq <steambuyer69@gmail.com>
2026-09-15 05:52:21 +0200

    fix: let compositor input wake DPMS after shell restart

--- modules/home/hyprland/settings.nix
+++ modules/home/hyprland/settings.nix
@@ -52,6 +52,9 @@
   };
 
   misc = {
+    # Compositor-owned wake also works after the shell restarts while idle.
+    key_press_enables_dpms = true;
+    mouse_move_enables_dpms = true;
     disable_hyprland_logo = true;
     disable_splash_rendering = true;
     force_default_wallpaper = 0;
